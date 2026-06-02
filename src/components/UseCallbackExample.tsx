46212f4e920464f97bb867681f9ecf20ba0809ca
Roma-723 <romajon.ll8@gmail.com>
2026-06-02 13:13:38 +0500

    name

--- src/components/UseCallbackExample.tsx
+++ src/components/UseCallbackExample.tsx
@@ -1,8 +1,16 @@
+import { useCallback, useState } from "react"
 
 const UseCallbackExample = () => {
+  const [state, setState] = useState(0)
+
+  const increament = useCallback(() => {
+    setState(state + 1)
+  }, [])
   return (
     <div>
-      
+     <p>+</p>
+     <p>{state}</p>
+     <p>-</p>
     </div>
   )
 }
