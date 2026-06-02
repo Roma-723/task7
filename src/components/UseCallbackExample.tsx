471a43001b3bffff68ecbba7353c73dc1edcac9f
Roma-723 <romajon.ll8@gmail.com>
2026-06-02 13:22:28 +0500

    name

--- src/components/UseCallbackExample.tsx
+++ src/components/UseCallbackExample.tsx
@@ -5,12 +5,17 @@ const UseCallbackExample = () => {
 
   const increament = useCallback(() => {
     setState(state + 1)
-  }, [])
+  }, [state])
+  const decreament = useCallback(() => {
+    setState(state - 1)
+  }, [state])
   return (
-    <div>
-     <p>+</p>
-     <p>{state}</p>
-     <p>-</p>
+    <div className="flex justify-center">
+      <div className="flex gap-10 ">
+        <p className="text-2xl cursor-pointer w-10 h-10 border items-center text-center rounded-[50%] " onClick={() => increament()}>+</p>
+        <p className="text-3xl font-bold">{state}</p>
+        <p className="text-2xl border w-10 h-10 items-center text-center rounded-[50%] cursor-pointer" onClick={() => decreament()}>-</p>
+      </div>
     </div>
   )
 }
