4335b480caf9183ab1b61f7a674c3ee8d9df8ec8
Roma-723 <romajon.ll8@gmail.com>
2026-06-03 13:15:03 +0500

    name

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react";
-
 function useLocalStorage<T>(key: string, initialValue: T) {
   const [value, setValue] = useState<T>(() => {
     const savedValue = localStorage.getItem(key);
-
     if (savedValue !== null) {
       return JSON.parse(savedValue);
     }
     return initialValue;
   });
-
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
