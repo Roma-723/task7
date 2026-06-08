3a9868c8e369bdd3a6ebd449b8ff18e75c55db50
Roma-723 <romajon.ll8@gmail.com>
2026-06-08 13:37:24 +0500

    firsName

--- src/components/UseCallbackExample.tsx
+++ src/components/UseCallbackExample.tsx
@@ -14,7 +14,6 @@ function App() {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-        
         <button className="bg-blue-800 w-[90%] ml-5 mt-5 h-10 rounded-[7px]" onClick={showName}>
           save
         </button>
