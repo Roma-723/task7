aca27c9152dec2992aea36410558d06cb8e6df3e
Roma-723 <romajon.ll8@gmail.com>
2026-06-08 09:21:32 +0500

    firsName

--- src/components/UseCallbackExample.tsx
+++ src/components/UseCallbackExample.tsx
@@ -1,27 +1,24 @@
-import { useCallback, useState } from "react"
+import { useState, useCallback } from "react";
 
-const UseCallbackExample = () => {
-  const [state, setState] = useState(1)
+function App() {
+  const [name, setName] = useState("");
 
-  const loop = useCallback((limit: number) => {
-    let result = ""
-    for (let i = 1; i <= limit; i++) {
-      result += i * i + " "
-    }
-    return result
-  }, [])
+  const showName = useCallback(() => {
+    alert(name);
+  }, [name]);
+  console.log("name");
 
   return (
-    <div className="flex flex-col items-center gap-4 p-6">
-      <p className="text-lg text-blue-400">{loop(state)}</p>
-      <button
-        onClick={() => setState(prev => prev + 1)}
-        className="px-4 py-2 bg-green-500 text-white rounded"
-      >
-        +1
+    <div>
+      <input placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      /> 
+      <button onClick={showName}>
+        Nishon dodani nom
       </button>
     </div>
-  )
+  );
 }
 
-export default UseCallbackExample
+export default App;
