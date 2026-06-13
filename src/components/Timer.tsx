bd096713477c3dddd1f8ffe81bca81ef5d209be4
Roma-723 <romajon.ll8@gmail.com>
2026-06-13 11:43:18 +0500

    firsName

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -23,6 +23,7 @@ const TimerExample = () => {
     stopTimer();
     setSeconds(0);
   };
+  
   const isRunning = intervalRef.current !== null;
   return (
     <div className="flex flex-col items-center gap-8 p-10">
